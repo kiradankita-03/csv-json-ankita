a74f47f0e67b60b305e9dd2e963bf25702d8b9d5
kiradankita-03 <ankitakirad3@gmail.com>
2020-01-15 14:56:37 +0530

    Commit

--- csv-json-ankita.py
+++ csv-json-ankita.py
@@ -1,1 +1,15 @@
+import csv
+import json
+csvFileP = "colour.csv"
+JsonFileP = "Jcolour.json"
+
+animal = {}
+with open(csvFileP)as csvF:
+    csvReader = csv.DictReader(csvF)
+    for csvRows in csvReader:
+        Colour = csvRows['Colour']
+        animal[Colour] = csvRows
+
+with open('JsonFileP', 'w') as jsonF:
+    jsonF.write(json.dumps(animal, indent=4))
 
